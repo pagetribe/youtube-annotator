e6e53c1d3e737cc4272773f099df695b1d288c9a
pagetribe <pagetribemail@gmail.com>
2015-11-02 21:45:38 +1100

    got message passing working

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
 // Listen for click on the icon
-chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.captureVisibleTab(function(img) {
-		alert(img);
+chrome.browserAction.onClicked.addListener(function(tab) {
+	// send a message to the active tab
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, {"message": "clicked_browser_action"});
 	});
+
+	// chrome.tabs.captureVisibleTab(function(img) {
+	// 	alert(img);
+	// });
 });
